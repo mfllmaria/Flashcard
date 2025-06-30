9b11916208218bc9e3b0464dff09a6872d539f2a
mfllmaria <maria.martins.lima04@escola.pr.gov.br>
2025-06-29 21:11:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const container = document.getElementById("container");
+
+perguntas.forEach(({ materia, pergunta, resposta }) => {
+  const card = document.createElement("div");
+  card.classList.add("cartao");
+
+  const conteudo = document.createElement("div");
+  conteudo.classList.add("cartao__conteudo");
 
