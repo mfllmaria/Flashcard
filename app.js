ad441f58f5e38e38e4ad3a94658bba0b92ae327e
mfllmaria <maria.martins.lima04@escola.pr.gov.br>
2025-06-29 21:11:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,23 @@ perguntas.forEach(({ materia, pergunta, resposta }) => {
 
   const conteudo = document.createElement("div");
   conteudo.classList.add("cartao__conteudo");
+  const frente = document.createElement("div");
+  frente.classList.add("face", "frente");
+  frente.innerHTML = `<h3>${materia}</h3><p>${pergunta}</p>`;
+
+  const verso = document.createElement("div");
+  verso.classList.add("face", "verso");
+  verso.innerHTML = `<p>${resposta}</p>`;
+
+  conteudo.appendChild(frente);
+  conteudo.appendChild(verso);
+  card.appendChild(conteudo);
+
+  card.addEventListener("click", () => {
+    card.classList.toggle("virado");
+  });
+
+  container.appendChild(card);
+});
+
 
