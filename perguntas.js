3200f0ddcf30311c316150bb135a90701b8fa999
mfllmaria <maria.martins.lima04@escola.pr.gov.br>
2025-06-29 21:10:53 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,48 @@
+const perguntas = [
+  {
+    materia: "Cachorros",
+    pergunta: "Qual raça é conhecida por ser extremamente inteligente?",
+    resposta: "Border Collie"
+  },
+  {
+    materia: "Gatos",
+    pergunta: "Por que os gatos ronronam?",
+    resposta: "Para demonstrar bem-estar ou se acalmar"
+  },
+  {
+    materia: "Aves",
+    pergunta: "Qual ave é famosa por repetir palavras humanas?",
+    resposta: "Papagaio"
+  },
+  {
+    materia: "Animais Marinhos",
+    pergunta: "Qual animal tem oito braços e esguicha tinta?",
+    resposta: "Polvo"
+  },
+  {
+    materia: "Répteis",
+    pergunta: "As cobras têm pernas?",
+    resposta: "Não, elas rastejam"
+  },
+  {
+    materia: "Animais Selvagens",
+    pergunta: "Qual é o maior felino do mundo?",
+    resposta: "Tigre-siberiano"
+  },
+  {
+    materia: "Insetos",
+    pergunta: "Qual inseto é essencial para a polinização?",
+    resposta: "Abelha"
+  },
+  {
+    materia: "Anfíbios",
+    pergunta: "Qual animal vive parte da vida na água e parte na terra?",
+    resposta: "Sapo"
+  },
+  {
+    materia: "Curiosidade",
+    pergunta: "Qual é o animal mais rápido do mundo?",
+    resposta: "Falcão-peregrino"
+  }
+];
 
